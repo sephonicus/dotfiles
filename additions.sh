0431a9060c800c37df6235372fa2cc5d52897d7e
Matthew Hassfurder <sephonicus@gmail.com>
2014-02-17 23:17:31 -0800

    Source helpers early for use in other additions

--- additions.sh
+++ additions.sh
@@ -1,8 +1,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # [1]
 
+source "${DIR}/helpers.sh"
 for addition in `ls $DIR/additions.d/*.sh`; do
   source $addition
 done
 
 # github.com/jqr/dotfiles
+
 # [1] stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
