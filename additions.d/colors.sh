72ea8859a1e24d702c2c65bbfaf8b0cf75a51600
Matthew Hassfurder <sephonicus@gmail.com>
2014-04-05 22:42:37 -0700

    Fix wrapping issues when using colors in prompt

--- additions.d/colors.sh
+++ additions.d/colors.sh
@@ -1,22 +1,22 @@
 # novell.com/coolsolutions/tools/17142.html
 # stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux/
-export BLACK='\033[0;30m'
-export BLUE='\033[0;34m'
-export GREEN='\033[0;32m'
-export CYAN='\033[0;36m'
-export RED='\033[0;31m'
-export PURPLE='\033[0;35m'
-export BROWN='\033[0;33m'
-export LIGHTGRAY='\033[0;37m'
-export DARKGRAY='\033[1;30m'
-export LIGHTBLUE='\033[1;34m'
-export LIGHTGREEN='\033[1;32m'
-export LIGHTCYAN='\033[1;36m'
-export LIGHTRED='\033[1;31m'
-export LIGHTPURPLE='\033[1;35m'
-export YELLOW='\033[1;33m'
-export WHITE='\033[1;37m'
-export NC='\033[0m' # no color
+export BLACK='\[\e[0;30m\]'
+export BLUE='\[\e[0;34m\]'
+export GREEN='\[\e[0;32m\]'
+export CYAN='\[\e[0;36m\]'
+export RED='\[\e[0;31m\]'
+export PURPLE='\[\e[0;35m\]'
+export BROWN='\[\e[0;33m\]'
+export LIGHTGRAY='\[\e[0;37m\]'
+export DARKGRAY='\[\e[1;30m\]'
+export LIGHTBLUE='\[\e[1;34m\]'
+export LIGHTGREEN='\[\e[1;32m\]'
+export LIGHTCYAN='\[\e[1;36m\]'
+export LIGHTRED='\[\e[1;31m\]'
+export LIGHTPURPLE='\[\e[1;35m\]'
+export YELLOW='\[\e[1;33m\]'
+export WHITE='\[\e[1;37m\]'
+export NC='\[\e[0m\]' # no color
 
 # osxdaily.com/2012/02/21/add-color-to-the-terminal-in-mac-os-x/
 export CLICOLOR=1
